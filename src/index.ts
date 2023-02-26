c739d90b38fe91b6eb3c2a5ed11a75366d1ad95d
StationaryStation <raulytstation@outlook.com>
2023-02-26 13:12:17 -0400

    mog

--- src/index.ts
+++ src/index.ts
@@ -5,22 +5,22 @@ const toolset = new Client();
 
 toolset.on("message", async (message) => {
     if (message.isUser() && message.channel.permissionsFor(message.member).has(Permissions.SendMessage)) {
+        console.log("message in", message.channel.name, `from ${message.author.username}:`, message.content);
         if (message.content === "/amogus") {
-            console.log("this should send a message", message.content);
             message.reply(":01G7MT5B978E360NB6VWAS9SJ6:")
         } else if (message.content === "/mogi about") {
-            const embed = new EmbedBuilder ({
+            const embed = new EmbedBuilder({
                 color: "#D8002E",
                 title: "mogi",
-                description: `Hostname: \`${os.hostname}\`\nMogi Version: \`1.0.1\`\nNode Version: \`${process.version}\`\nTypescript Version: \`${(require("../package.json").dependencies.typescript as string).substring(1,6)}\``,
+                description: `Hostname: \`${os.hostname}\`\nMogi Version: \`1.0.1\`\nNode Version: \`${process.version}\`\nTypescript Version: \`${(require("../package.json").dependencies.typescript as string).substring(1, 6)}\``,
                 iconURL: "https://autumn.revolt.chat/emojis/01G7MT5B978E360NB6VWAS9SJ6"
             })
             message.reply(embed)
         } else {
-            console.log("this should not send a message", message.content);
+            console.log("this should not send a message");
         }
     } else {
-        console.log("not a user", message)
+        console.log("not a user")
     }
 })
 
