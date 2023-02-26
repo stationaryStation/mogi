34af5d69bbe5082871aa573ac346fa2ebfe7af57
StationaryStation <raulytstation@outlook.com>
2023-02-26 12:04:49 -0400

    fix gitignore + add about

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
-import { Client, Permissions } from "revolt-toolset"
+import { Client, EmbedBuilder, Permissions } from "revolt-toolset"
+import os from "node:os";
 
 const toolset = new Client();
 
 toolset.on("message", async (message) => {
-    if (message.isUser()) {
-        if (message.channel.permissionsFor(message.member).has(Permissions.SendMessage) && message.content === "/amogus") {
+    if (message.isUser() && message.channel.permissionsFor(message.member).has(Permissions.SendMessage)) {
+        if (message.content === "/amogus") {
             console.log("this should send a message", message.content);
             message.reply(":01G7MT5B978E360NB6VWAS9SJ6:")
+        } else if (message.content === "/mogi about") {
+            const embed = new EmbedBuilder ({
+                color: "#D8002E",
+                title: "mogi",
+                description: `Hostname: \`${os.hostname}\`\nMogi Version: \`1.0.1\`\nNode Version: \`${process.version}\`\nTypescript Version: \`${(require("../package.json").dependencies.typescript as string).substring(1,6)}\``,
+                iconURL: "https://autumn.revolt.chat/emojis/01G7MT5B978E360NB6VWAS9SJ6"
+            })
+            message.reply(embed)
         } else {
             console.log("this should not send a message", message.content);
         }
